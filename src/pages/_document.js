086c1d8a0e0d42824bbd630dcd496bb201fc1c9d
Sakilluhar <sakilluharpc@gmail.com>
2023-10-09 15:45:16 +0530

    9-10 3:45

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,7 +37,7 @@ const CustomDocument = ({ ids, css }) => {
         <Main />
         <NextScript />
         
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script>
+        {/* <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script> */}
       </body>
     </Html>
   );
